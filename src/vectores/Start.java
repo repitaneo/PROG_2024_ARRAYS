e6a0dc366caf049bd3d7d29c4390d7c286375528
Marcos <Marcos@Profesor>
2024-01-09 09:46:17 +0100

    segundo cambio: el start ya no tiene datos

--- src/vectores/Start.java
+++ src/vectores/Start.java
@@ -9,17 +9,7 @@ public class Start {
 
 		char frase[] = new char[10];
 		
-		frase[0] = 'H';
-		frase[1] = 'o';
-		frase[2] = 'l';
-		frase[3] = 'a';
-		frase[4] = ' ';
-		frase[5] = 'h';
-		frase[6] = 'o';
-		frase[7] = 'l';
-		frase[8] = 'a';
-		frase[9] = '!';
-		
+	
 		for(int i=0;i<frase.length;i++) {
 			// System.out.println("pos "+i+" = "+frase[i]);
 			System.out.print(frase[i]);
